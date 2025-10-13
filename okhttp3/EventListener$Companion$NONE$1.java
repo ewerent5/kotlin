409086b0ef6b308bcb2c0652fc44f143267003b8
Alex <razor3726@gmail.com>
2025-10-13 15:19:54 +0300

    Delete okhttp3 directory

--- okhttp3/EventListener$Companion$NONE$1.java
+++
@@ -1,8 +0,0 @@
-package okhttp3;
-
-/* compiled from: EventListener.kt */
-/* loaded from: classes3.dex */
-public final class EventListener$Companion$NONE$1 extends EventListener {
-    EventListener$Companion$NONE$1() {
-    }
-}
--- okhttp3/a.java
+++
@@ -1,8 +0,0 @@
-package okhttp3;
-
-/* loaded from: classes3.dex */
-public /* synthetic */ class a {
-    public static /* synthetic */ int a(boolean z) {
-        return z ? 1231 : 1237;
-    }
-}
